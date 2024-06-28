bc8c33aea747548e4cbcc52006d9f80bbe5f5e8b
banalok <alokkbh26@gmail.com>
2024-06-28 10:55:25 -0400

    agent cuda check

--- patchless_nnunet/predict.py
+++ patchless_nnunet/predict.py
@@ -18,189 +18,201 @@ from clearml import Task
 from patchless_nnunet import utils, setup_root
 import os
 
-log = utils.get_pylogger(__name__)
-hydra.initialize_config_dir("F:\clearml_cardioai\patchless_nnunet\configs")
-cfg = hydra.compose(config_name="predict")
-class PatchlessPreprocessd(MapTransform):
-    """Load and preprocess data path given in dictionary keys.
+task = Task.init(project_name="check_gpu", task_name="cuda_check") 
+task.execute_remotely(queue_name="default", exit_process=True)
 
-    Dictionary must contain the following key(s): "image" and/or "label".
-    """
-
-    def __init__(
-        self, keys, common_spacing,
-    ) -> None:
-        """Initialize class instance.
-
-        Args:
-            keys: Keys of the corresponding items to be transformed.
-            common_spacing: Common spacing to resample the data.
-        """
-        super().__init__(keys)
-        self.keys = keys
-        self.common_spacing = np.array(common_spacing)
-
-    def __call__(self, data: dict[str, str]):
-        # load data
-        d = dict(data)
-        image = d["image"]
-
-        image_meta_dict = {"case_identifier": os.path.basename(image._meta["filename_or_obj"]),
-                           "original_shape": np.array(image.shape[1:]),
-                           "original_spacing": np.array(image._meta["pixdim"][1:4].tolist())}
-        original_affine = np.array(image._meta["original_affine"].tolist())
-        image_meta_dict["original_affine"] = original_affine
-
-        image = image.cpu().detach().numpy()
-        # transforms and resampling
-        if self.common_spacing is None:
-            raise Exception("COMMON SPACING IS NONE!")
-        transform = tio.Resample(self.common_spacing)
-        resampled = transform(tio.ScalarImage(tensor=image, affine=original_affine))
-
-        croporpad = tio.CropOrPad(self.get_desired_size(resampled.shape[1:]))
-        resampled_cropped = croporpad(resampled)
-        resampled_affine = resampled_cropped.affine
-
-        d["image"] = resampled_cropped.numpy().astype(np.float32)
-
-        image_meta_dict['resampled_affine'] = resampled_affine
-
-        d["image_meta_dict"] = image_meta_dict
-        return d
-
-    def get_desired_size(self, current_shape, divisible_by=(32, 32, 4)):
-        # get desired closest divisible bigger shape
-        x = int(np.ceil(current_shape[0] / divisible_by[0]) * divisible_by[0])
-        y = int(np.ceil(current_shape[1] / divisible_by[1]) * divisible_by[1])
-        z = current_shape[2]
-        return x, y, z
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
+print("Running nvidia-smi command:")
+os.system("nvidia-smi")
+    
+print("\nRunning nvcc --version command:")
+os.system("nvcc --version")
 
 
-class PatchlessnnUnetPredictor:
-    @classmethod
-    def main(cls) -> None:
-        """Runs the requested experiment."""
-        # Set up the environment
-        cls.pre_run_routine()
+# log = utils.get_pylogger(__name__)
+# hydra.initialize_config_dir("F:\clearml_cardioai\patchless_nnunet\configs")
+# cfg = hydra.compose(config_name="predict")
+# class PatchlessPreprocessd(MapTransform):
+#     """Load and preprocess data path given in dictionary keys.
 
-        # Run the system with config loaded by @hydra.main
-        cls.run_system(cfg)
+#     Dictionary must contain the following key(s): "image" and/or "label".
+#     """
 
-    @classmethod
-    def pre_run_routine(cls) -> None:
-        """Sets-up the environment before running the training/testing."""
-        # Load environment variables from `.env` file if it exists
-        # Load before hydra main to allow for setting environment variables with ${oc.env:ENV_NAME}
-        setup_root()
+#     def __init__(
+#         self, keys, common_spacing,
+#     ) -> None:
+#         """Initialize class instance.
 
-    @staticmethod
-    def load_and_prepare_nifti_img(img_path: Path):
-        nifti_img = nib.load(img_path)
+#         Args:
+#             keys: Keys of the corresponding items to be transformed.
+#             common_spacing: Common spacing to resample the data.
+#         """
+#         super().__init__(keys)
+#         self.keys = keys
+#         self.common_spacing = np.array(common_spacing)
 
-        data = np.expand_dims(nifti_img.get_fdata(), 0)
-        hdr = nifti_img.header
-        aff = nifti_img.affine
+#     def __call__(self, data: dict[str, str]):
+#         # load data
+#         d = dict(data)
+#         image = d["image"]
 
-        meta = {"filename_or_obj": img_path.stem.split('.')[0].strip("_0000"),
-                "pixdim": hdr['pixdim'],
-                "original_affine": aff}
-        return {'image': MetaTensor(torch.tensor(data, dtype=torch.float32), meta=meta)}
+#         image_meta_dict = {"case_identifier": os.path.basename(image._meta["filename_or_obj"]),
+#                            "original_shape": np.array(image.shape[1:]),
+#                            "original_spacing": np.array(image._meta["pixdim"][1:4].tolist())}
+#         original_affine = np.array(image._meta["original_affine"].tolist())
+#         image_meta_dict["original_affine"] = original_affine
 
-    @staticmethod
-    def get_array_dataset(input_path):
-        tensor_list = []
-        # find all nifti files in input_path
-        # open and get relevant information
-        # add to list of data
-        input_path = Path(input_path)
-        if input_path.is_file():
-            log.info(f"Predicting on file: {input_path}")
-            tensor_list += [PatchlessnnUnetPredictor.load_and_prepare_nifti_img(input_path)]
-        else:
-            log.info(f"Predicting on file list found recursively in folder: {input_path}")
-            for nifti_file_p in input_path.rglob('*.nii.gz'):
-                tensor_list += [PatchlessnnUnetPredictor.load_and_prepare_nifti_img(nifti_file_p)]
-        return tensor_list
+#         image = image.cpu().detach().numpy()
+#         # transforms and resampling
+#         if self.common_spacing is None:
+#             raise Exception("COMMON SPACING IS NONE!")
+#         transform = tio.Resample(self.common_spacing)
+#         resampled = transform(tio.ScalarImage(tensor=image, affine=original_affine))
 
-    @staticmethod
-    #@hydra.main(version_base="1.3", config_path="configs", config_name="predict")
-    @utils.task_wrapper
-    def run_system(cfg: DictConfig) -> Tuple[dict, dict]:
-        """Predict unseen cases with a given checkpoint.
+#         croporpad = tio.CropOrPad(self.get_desired_size(resampled.shape[1:]))
+#         resampled_cropped = croporpad(resampled)
+#         resampled_affine = resampled_cropped.affine
 
-        Currently, this method only supports inference for nnUNet models.
+#         d["image"] = resampled_cropped.numpy().astype(np.float32)
 
-        This method is wrapped in optional @task_wrapper decorator, that controls the behavior during
-        failure. Useful for multiruns, saving info about the crash, etc.
+#         image_meta_dict['resampled_affine'] = resampled_affine
 
-        Args:
-            cfg (DictConfig): Configuration composed by Hydra.
+#         d["image_meta_dict"] = image_meta_dict
+#         return d
 
-        Returns:
-            Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
+#     def get_desired_size(self, current_shape, divisible_by=(32, 32, 4)):
+#         # get desired closest divisible bigger shape
+#         x = int(np.ceil(current_shape[0] / divisible_by[0]) * divisible_by[0])
+#         y = int(np.ceil(current_shape[1] / divisible_by[1]) * divisible_by[1])
+#         z = current_shape[2]
+#         return x, y, z
 
-        Raises:
-            ValueError: If the checkpoint path is not provided.
-        """
 
-        # Initialize ClearML task
-        #Task.create(repo="https://github.com/banalok/clearml_cardioai.git")
-        #Task.force_requirements_env_freeze(requirements_file="requirements.txt")
-        task = Task.init(project_name="local_prediction", task_name="local_inference") 
-        task.set_repo(repo="https://github.com/banalok/clearml_cardioai.git", branch="main")       
-        task.execute_remotely(queue_name="default", exit_process=True)
-        # apply extra utilities
-        # (e.g. ask for tags if none are provided in cfg, print cfg tree, etc.)
-        #utils.extras(cfg)
+# class PatchlessnnUnetPredictor:
+#     @classmethod
+#     def main(cls) -> None:
+#         """Runs the requested experiment."""
+#         # Set up the environment
+#         cls.pre_run_routine()
 
-        if not cfg.ckpt_path:
-            raise ValueError("ckpt_path must not be empty!")
+#         # Run the system with config loaded by @hydra.main
+#         cls.run_system(cfg)
+
+#     @classmethod
+#     def pre_run_routine(cls) -> None:
+#         """Sets-up the environment before running the training/testing."""
+#         # Load environment variables from `.env` file if it exists
+#         # Load before hydra main to allow for setting environment variables with ${oc.env:ENV_NAME}
+#         setup_root()
+
+#     @staticmethod
+#     def load_and_prepare_nifti_img(img_path: Path):
+#         nifti_img = nib.load(img_path)
+
+#         data = np.expand_dims(nifti_img.get_fdata(), 0)
+#         hdr = nifti_img.header
+#         aff = nifti_img.affine
+
+#         meta = {"filename_or_obj": img_path.stem.split('.')[0].strip("_0000"),
+#                 "pixdim": hdr['pixdim'],
+#                 "original_affine": aff}
+#         return {'image': MetaTensor(torch.tensor(data, dtype=torch.float32), meta=meta)}
+
+#     @staticmethod
+#     def get_array_dataset(input_path):
+#         tensor_list = []
+#         # find all nifti files in input_path
+#         # open and get relevant information
+#         # add to list of data
+#         input_path = Path(input_path)
+#         if input_path.is_file():
+#             log.info(f"Predicting on file: {input_path}")
+#             tensor_list += [PatchlessnnUnetPredictor.load_and_prepare_nifti_img(input_path)]
+#         else:
+#             log.info(f"Predicting on file list found recursively in folder: {input_path}")
+#             for nifti_file_p in input_path.rglob('*.nii.gz'):
+#                 tensor_list += [PatchlessnnUnetPredictor.load_and_prepare_nifti_img(nifti_file_p)]
+#         return tensor_list
+
+#     @staticmethod
+#     #@hydra.main(version_base="1.3", config_path="configs", config_name="predict")
+#     @utils.task_wrapper
+#     def run_system(cfg: DictConfig) -> Tuple[dict, dict]:
+#         """Predict unseen cases with a given checkpoint.
+
+#         Currently, this method only supports inference for nnUNet models.
+
+#         This method is wrapped in optional @task_wrapper decorator, that controls the behavior during
+#         failure. Useful for multiruns, saving info about the crash, etc.
+
+#         Args:
+#             cfg (DictConfig): Configuration composed by Hydra.
+
+#         Returns:
+#             Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
+
+#         Raises:
+#             ValueError: If the checkpoint path is not provided.
+#         """
+
+#         # Initialize ClearML task
+#         #Task.create(repo="https://github.com/banalok/clearml_cardioai.git")
+#         #Task.force_requirements_env_freeze(requirements_file="requirements.txt")
+#         task = Task.init(project_name="local_prediction", task_name="local_inference") 
+#         task.set_repo(repo="https://github.com/banalok/clearml_cardioai.git", branch="main")       
+#         task.execute_remotely(queue_name="default", exit_process=True)
+#         # apply extra utilities
+#         # (e.g. ask for tags if none are provided in cfg, print cfg tree, etc.)
+#         #utils.extras(cfg)
+
+#         if not cfg.ckpt_path:
+#             raise ValueError("ckpt_path must not be empty!")
         
-        log.info(f"Instantiating model <{cfg.model._target_}>")
-        model: LightningModule = hydra.utils.instantiate(cfg.model)
+#         log.info(f"Instantiating model <{cfg.model._target_}>")
+#         model: LightningModule = hydra.utils.instantiate(cfg.model)
 
-        log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
-        trainer: Trainer = hydra.utils.instantiate(cfg.trainer)
+#         log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
+#         trainer: Trainer = hydra.utils.instantiate(cfg.trainer)
 
-        object_dict = {
-            "cfg": cfg,
-            "model": model,
-            "trainer": trainer,
-        }
+#         object_dict = {
+#             "cfg": cfg,
+#             "model": model,
+#             "trainer": trainer,
+#         }
 
-        preprocessed = PatchlessPreprocessd(keys='image', common_spacing=cfg.model.common_spacing)
-        tf = transforms.compose.Compose([preprocessed, ToTensord(keys="image", track_meta=True)])
+#         preprocessed = PatchlessPreprocessd(keys='image', common_spacing=cfg.model.common_spacing)
+#         tf = transforms.compose.Compose([preprocessed, ToTensord(keys="image", track_meta=True)])
 
-        numpy_arr_data = PatchlessnnUnetPredictor.get_array_dataset(cfg.input_folder)
-        dataset = ArrayDataset(img=numpy_arr_data, img_transform=tf)
+#         numpy_arr_data = PatchlessnnUnetPredictor.get_array_dataset(cfg.input_folder)
+#         dataset = ArrayDataset(img=numpy_arr_data, img_transform=tf)
 
-        dataloader = DataLoader(
-            dataset=dataset,
-            batch_size=1,
-            num_workers=cfg.num_workers,
-            pin_memory=cfg.pin_memory,
-            shuffle=False,
-        )
+#         dataloader = DataLoader(
+#             dataset=dataset,
+#             batch_size=1,
+#             num_workers=cfg.num_workers,
+#             pin_memory=cfg.pin_memory,
+#             shuffle=False,
+#         )
 
-        log.info("Starting predicting!")
-        log.info(f"Using checkpoint: {cfg.ckpt_path}")
-        trainer.predict(model=model, dataloaders=dataloader, ckpt_path=cfg.ckpt_path)
+#         log.info("Starting predicting!")
+#         log.info(f"Using checkpoint: {cfg.ckpt_path}")
+#         trainer.predict(model=model, dataloaders=dataloader, ckpt_path=cfg.ckpt_path)
 
-        metric_dict = trainer.callback_metrics
+#         metric_dict = trainer.callback_metrics
 
-        for key, value in metric_dict.items():
-          task.get_logger().report_scalar(key, value)
+#         for key, value in metric_dict.items():
+#           task.get_logger().report_scalar(key, value)
 
-        task.connect(object_dict)           
+#         task.connect(object_dict)           
                 
-        return metric_dict, object_dict
+#         return metric_dict, object_dict
 
 
-def main():
-    """Run the script."""
-    PatchlessnnUnetPredictor.main()
+# def main():
+#     """Run the script."""
+#     PatchlessnnUnetPredictor.main()
 
 
-if __name__ == '__main__':
-    PatchlessnnUnetPredictor.main()
+# if __name__ == '__main__':
+#     PatchlessnnUnetPredictor.main()
